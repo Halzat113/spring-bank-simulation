ec4d245a832bc1635ff5d53c51bc1925b365d61a
halzathalik <halzathalik@gmail.com>
2023-12-22 23:14:05 -0500

    limit issue fixed in transancionRepository

--- src/main/java/com/example/repository/TransactionRepository.java
+++ src/main/java/com/example/repository/TransactionRepository.java
@@ -1,20 +1,12 @@
 package com.example.repository;
-
-import com.example.dto.TransactionDto;
-import com.example.entity.Account;
 import com.example.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Component;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
-    //TODO limit 10 needs to be fixed (having error by doing 'limit by 10')
-    @Query("SELECT t from Transaction t order by t.createDate")
+    @Query(value = "select * from transactions order by create_date limit 10;",nativeQuery = true)
     List<Transaction> findLast10Transactions();
 
     @Query("SELECT t from Transaction t where t.sender='?1' or t.receiver='?1'")
