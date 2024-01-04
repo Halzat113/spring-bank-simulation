6bb97d30be79258ab048eecfc8269f2dc0fe41b7
halzathalik <halzathalik@gmail.com>
2024-01-04 14:33:36 -0500

    security completed

---
+++ src/main/java/com/example/Test.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class Test {
+    static PasswordEncoder passwordEncoder;
+
+
+
+}
